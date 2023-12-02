67028e178f03cb60f430b5a6587b68b17aa139a7
Roman Paholík <wizzardsk@gmail.com>
2023-12-02 23:22:08 +0100

    remove script

--- Named_Snaps/resize.sh
+++
@@ -1,9 +0,0 @@
-find . -type f -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -exec sh -c 'identify -format "%w %i\n" "$0" | {
-  read width file
-  if [ "$width" -gt 512 ]; then
-    convert "$file" -resize 320x "$file"
-    echo "Zmenšený súbor: $file"
-  else
-    echo "Preskočený súbor: $file"
-  fi
-}' {} \;
